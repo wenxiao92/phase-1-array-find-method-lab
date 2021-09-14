54e882fca418ceac23c8f37dfc6382312add529d
Wen Xiao <wennstyle@gmail.com>
2021-09-14 00:02:42 -0400

    commit tree 1530d955896f78bc7b2d98f2f8850d80bb83c722

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+//function superbowlWin(historicData) {
+//    for (const year of historicData) {
+//        if (historicData[year][1] === 'W') {
+//            return historicData[year][1];
+//        }
+//    }
+//}
 
+  function superbowlWin(array) { //takes in the array as an argument
+    for (const listOfData of array) { //loops through each element with listofData as the list of indexes for the outer array
+      if (listOfData.result === "W") { //looks through the element and logs the position of that element when the element if/when found
+        return listOfData.year //returns whatever we want based on the position logged in the condition
+      }
+    }
+  }
